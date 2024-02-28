bf989dd31ea325d72f380d8053e4925ff0022122
Dalden <daldenuteryag@gmail.com>
2024-02-28 17:50:13 +0600

    123

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'localhost:3000',
+  origin: 'http://localhost:3000',
   credentials: true // Если ваш фронтенд отправляет запросы с куками или заголовками авторизации
 }));
 
